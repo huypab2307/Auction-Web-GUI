f65adc67214c03c43576b87f8d3a5f4a35a945c1
DuongMikey <dongduong828@gmail.com>
2026-04-06 08:25:07 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("hello i am jenny huynh");
+        for (int i = 0; i <  10; i++){
+            System.out.println(i);
+        }
     }
 }
