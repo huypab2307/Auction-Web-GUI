08ee197fa4cdea5be7fa593e4dbd1c1164a1ea7b
DuongMikey <dongduong828@gmail.com>
2026-04-08 13:36:34 +0700

    adjust

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,10 @@
-public class Product {
+public class Item {
     private String name;
     private String description;
     private Double price;
     private Double curPrice;
 
-    public Product(String name, String description, Double price) {
+    public Item(String name, String description, Double price) {
         this.name = name;
         this.description = description;
         this.price = price;
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,11 @@
 public abstract class Person {
     private String username;
     private String password;
-    private String id;
+    private static int id = 25020000;
 
-    public Person(String username, String password, String id) {
+    public Person(String username, String password) {
         this.username = username;
         this.password = password;
-        this.id = id;
+        this.id = Person.id++;
     }
 }
