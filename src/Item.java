96008bdebab2a141c4bf4221b514471a7428cc86
huypab2307 <huy181230@gmail.com>
2026-04-15 15:57:47 +0700

    Class: Item, Main
    Fix Bugs

--- src/Item.java
+++ src/Item.java
@@ -2,16 +2,16 @@ abstract class Item {
     private String name;
     private String description;
     private Double price;
-    private String seller_id;
+    private String sellerId;
     private static int cur_id = 00000; 
     private String id; //auto-generated
 
-    public Item(String name, String description, Double price,String type, String seller_id) {
+    public Item(String name, String description, Double price,String type, String sellerId) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.id = type + cur_id++;
-        this.seller_id = seller_id;
+        this.sellerId = sellerId;
     }
     public String getName() {
         return name;
@@ -25,14 +25,14 @@ abstract class Item {
         return price;
     }
 
-    public String getSeller_id() {
-        return seller_id;
+    public String getSellerId() {
+        return sellerId;
     }
 
     public static int getCur_id() {
         return cur_id;
     }
-    
+
     public String getId() {
         return id;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ class Main{
                         .withArtist("van goth")
                         .withDimensions("500 x 400")
                         .build();
-        System.out.println(art.getArtist());
+        System.out.println(art);
 
         Electronics electronic = FactoryItem.createBuilder(Electronics.Builder.class, "Fridge", "Day la cai tu lanh", 6767, "FR01")
                         .withBrand("Samsung")
@@ -15,7 +15,7 @@ class Main{
                         .withVoltage(300)
                         .withWeight(100)
                         .build();
-        System.out.println(electronic.getBrand());
+        System.out.println(electronic);
 
         Vehicle vehicle = FactoryItem.createBuilder(Vehicle.Builder.class, "SH","This is an SH", 1000, "V001")
                         .withMileage(8463)
@@ -23,6 +23,18 @@ class Main{
                         .withTitleStatus("Old")
                         .withTrim("Limited")
                         .build();
-        System.out.println(vehicle.getTitleStatus());
+        System.out.println(vehicle);
+
+        User u1 = UserFactory.createUser(Role.BIDDER, "an123", "123");
+        User u2 = UserFactory.createUser(Role.SELLER, "binh456", "456");
+        User u3 = UserFactory.createUser(Role.ADMIN, "admin", "admin");
+
+        u1.showRole();
+        u2.showRole();
+        u3.showRole();
+
+        User u = UserFactory.createUser(Role.BIDDER, "an", "123");
+        u = u.changeRole(Role.SELLER);
+        u.showRole();
     }
 }
