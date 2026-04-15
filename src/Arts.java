97bccbe13a90c22f0afd183588262ad26e6a81ec
DuongMikey <dongduong828@gmail.com>
2026-04-15 13:53:36 +0700

    Update Arts.java

--- src/Arts.java
+++ src/Arts.java
@@ -1,50 +1,65 @@
 public class Arts extends Item {
     private String artist; //Tác giả
-    private String yearOfcreation; //Năm sáng tác
+    private int yearOfcreation; //Năm sáng tác
     private String dimensions; //Kích thước
     private String medium; //Chất liệu 
-    public Arts (String name, String description, Double price,String seller_id, String artist, String yearOfcreation, String dimensions, String medium) {
-        super(name, description, price, "A", seller_id);
-        this.artist = artist;
-        this.yearOfcreation = yearOfcreation;
-        this.dimensions = dimensions;
-        this.medium = medium;
-    }
-    public Arts(String name, String description, double price, String seller_id){
-        super(name, description, price, "A", seller_id);
+
+    public Arts(Builder build){
+        super(build.name, build.description, build.price,"A", build.seller_id);
+        this.artist = build.artist;
+        this.yearOfcreation = build.yearOfCreation;
+        this.dimensions = build.dimensions;
+        this.medium = build.medium;
     }
     // Artist
     public String getArtist(){
         return this.artist;
     }
 
-    public void setArtist(String artist){
-        this.artist = artist;
-    }
     // YearOfCreation
     public String getYearOfcreation(){
         return this.yearOfcreation;
     }
     
-    public void setYearOfcreation(String yearOfcreation){
-        this.yearOfcreation = yearOfcreation;
-    }
     // Dimensions
     public String getDimensions(){
         return this.dimensions;
     }
 
-    public void setDimensions(String dimensions){
-        this.dimensions = dimensions;
-    }
     // Medium
     public String getMedium(){
         return this.medium;
     }
+    static private class Builder{
+        private String name;
+        private String description;
+        private double price;
+        private String seller_id;
+        private String artist; //Tác giả
+        private int yearOfCreation; //Năm sáng tác
+        private String dimensions; //Kích thước
+        private String medium; //Chất liệu 
+        public Builder(String name, String description, double price, String seller_id){
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.seller_id = seller_id;
+        }
+        public void withArtist(String name){
+            this.artist = name;
+        }
+        public void withyearOfCreation(int year ){
+            this.yearOfCreation = year;
+        }
+        public void withDimensions(String dimensions){
+            this.dimensions = dimensions;
+        }
+        public void medium(String medium){
+            this.medium = medium;
+        }
 
-    public void setMedium(String medium){
-        this.medium = medium;
+        public Item build(){
+            return new Arts(this);
+        }
     }
-
-    
 }
