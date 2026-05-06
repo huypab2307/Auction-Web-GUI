3038ff73a747242a415b53ca5b5f7167cdf9de21
DuongMikey <dongduong828@gmail.com>
2026-05-06 12:48:06 +0700

    notification added

--- auction-client/src/main/java/com/mikey/auction/javagui/topbar/NotificationController.java
+++ auction-client/src/main/java/com/mikey/auction/javagui/topbar/NotificationController.java
@@ -2,8 +2,14 @@ package com.mikey.auction.javagui.topbar;
 
 import com.mikey.auction.auction.Notifications;
 
+import com.mikey.auction.database.AuctionDAO;
+import com.mikey.auction.database.UserDAO;
+import com.mikey.auction.dto.AuctionInfo;
+import com.mikey.auction.javagui.SceneChanger;
+import com.mikey.auction.user.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import java.time.format.DateTimeFormatter;
 
@@ -13,15 +19,23 @@ public class NotificationController {
     @FXML private Text mainContent;
     @FXML private Label timeLabel;
     private int auctionId;
+    private int userId;
 
     @FXML
     public void setContent(Notifications notifications) {
         mainContent.setText(notifications.getMessage());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm-ss");
         String formattedTime = notifications.getCreatedAt().format(formatter);
+        userId = notifications.getUserId();
         timeLabel.setText(formattedTime);
         auctionId = notifications.getAuctionId(); 
     }
 
 
+    public void handleCardClick(MouseEvent mouseEvent) {
+        User user = UserDAO.getInstance().findById(userId);
+        AuctionInfo auctionInfo = AuctionDAO.getInstance().searchAuctionById(auctionId);
+        SceneChanger.getInstance().toAuction(auctionInfo, user.getId());
+
+    }
 }
