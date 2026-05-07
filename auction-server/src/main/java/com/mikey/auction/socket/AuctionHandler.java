58ec38a23ae36e7f05c72310aa3fd67dd0782e93
25020192-debug <25020192@vnu.edu.vn>
2026-05-07 22:08:55 +0700

    AuctionHandler edited

--- auction-server/src/main/java/com/mikey/auction/socket/AuctionHandler.java
+++ auction-server/src/main/java/com/mikey/auction/socket/AuctionHandler.java
@@ -1,16 +1,14 @@
 package com.mikey.auction.socket;
 
-import com.google.gson.Gson;
-import com.mikey.auction.manager.AuctionManager;
-import com.mikey.auction.database.AuctionDAO;
-import com.mikey.auction.dto.AuctionInfo;
-
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
+import com.google.gson.Gson;
+import com.mikey.auction.database.AuctionDAO;
 import com.mikey.auction.items.ItemType;
+import com.mikey.auction.manager.AuctionManager;
 
 public class AuctionHandler {
+    private static String response;
     private static final Gson gson = new Gson();
     public static void handleAuction(String message, PrintWriter out) {
         try {
@@ -20,42 +18,32 @@ public class AuctionHandler {
                     // Xử lý dữ liệu đấu giá (ví dụ: lưu vào database, cập nhật trạng thái, v.v.)
                     switch (auctionData) {
                         case "All":
-                            ArrayList<AuctionInfo> p = AuctionManager.getInstance().auctionList();
-                            out.println("AUCTION|ALL|" + gson.toJson(p));
-                            out.flush();
+                            response = "AUCTION|ALL|" + gson.toJson(AuctionManager.getInstance().auctionList());
                             break;
                         case "TYPE ARTS":
-                            ArrayList<AuctionInfo> p1 = AuctionDAO.getInstance().getAuctionsType(ItemType.ARTS);
-                            out.println("AUCTION|TYPE ARTS|" + gson.toJson(p1));
-                            out.flush();
+                            response = "AUCTION|TYPE ARTS|" + gson.toJson(AuctionDAO.getInstance().getAuctionsType(ItemType.ARTS));
                             break;
                         case "TYPE VEHICLE":
-                            ArrayList<AuctionInfo> p2 = AuctionDAO.getInstance().getAuctionsType(ItemType.VEHICLE);
-                            out.println("AUCTION|TYPE VEHICLE|" + gson.toJson(p2));
-                            out.flush();
+                            response = "AUCTION|TYPE VEHICLE|" + gson.toJson(AuctionDAO.getInstance().getAuctionsType(ItemType.VEHICLE));
                             break;
                         case "TYPE ELECTRONICS":
-                            ArrayList<AuctionInfo> p3 = AuctionDAO.getInstance().getAuctionsType(ItemType.ELECTRONICS);
-                            out.println("AUCTION|TYPE ELECTRONICS|" + gson.toJson(p3));
-                            out.flush();
+                            response = "AUCTION|TYPE ELECTRONICS|" + gson.toJson(AuctionDAO.getInstance().getAuctionsType(ItemType.ELECTRONICS));
                             break;
                         case "USER":
-                            int userid = gson.fromJson(parts[2].trim(), int.class);  
-                            ArrayList<AuctionInfo> p4 = AuctionDAO.getInstance(). searchAuctionByUserId(userid);
-                            out.println("AUCTION|USER|" + gson.toJson(p4));
-                            out.flush();
+                            int userid = gson.fromJson(parts[2].trim(), int.class); 
+                            response = "AUCTION|USER|" + gson.toJson(AuctionDAO.getInstance().searchAuctionByUserId(userid)); 
                             break;
                         case "SEARCH":
                             String keyword = gson.fromJson(parts[2].trim(), String.class); 
-                            ArrayList<AuctionInfo> p5 = AuctionDAO.getInstance().searchAuction(keyword);
-                            out.println("AUCTION|SEARCH|" + gson.toJson(p5));
-                            out.flush();
-                            break;
-                        
+                            response = "AUCTION|SEARCH|" + gson.toJson(AuctionDAO.getInstance().searchAuction(keyword));
+                            break;   
                     }
-                   
-
-            
+                    if (!response.isEmpty()) {
+                synchronized (out) { 
+                    out.println(response);
+                    out.flush();
+                }
+            }
         }
     } catch (Exception e) {
             out.println("ERROR");
